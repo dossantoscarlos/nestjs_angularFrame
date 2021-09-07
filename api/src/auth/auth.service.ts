0b87ef29e573c366fce3e74f6a747642a43192c6
Carlos Eduardo <anjoazrael10@gmail.com>
2021-09-07 11:56:09 -0300

    feat(hashPassword): adicionando criptografia - Add Lib bcrypt - Criptografia adicionada para senha.

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -6,15 +6,7 @@ export class AuthService {
   constructor(private usuarioService: UsuarioService) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
-    const usuarios = await this.usuarioService.findOne(email);
-    usuarios.forEach((usuario) => {
-      if (usuario && usuario.password === pass) {
-        const { ...result } = usuario;
-        return result;
-      } else {
-        return null;
-      }
-    });
+    const usuarios = await this.usuarioService.auth(email, pass);
     return usuarios;
   }
 }
--- api/src/usuario/usuario.service.ts
+++ api/src/usuario/usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Usuario } from './usuario.entity';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsuarioService {
   constructor(
@@ -13,7 +13,34 @@ export class UsuarioService {
     return this.usuarioRepository.find();
   }
 
-  async findOne(username: string): Promise<Usuario[]> {
-    return this.usuarioRepository.find({ where: { email: username } });
+  async auth(username: string, pass: string): Promise<Usuario> {
+    const find = await this.usuarioRepository.find({
+      where: { email: username },
+      take: 1,
+    });
+
+    const findOne = await this.usuarioRepository.findOne(find[0].id);
+    const usuario = { ...findOne };
+    const validaPassword = await this.comparePassword(pass, usuario.password);
+
+    if (usuario && validaPassword) {
+      const { ...result } = usuario;
+      return result;
+    } else {
+      return null;
+    }
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    const hash = await bcrypt.hash(password, 10);
+    return hash;
+  }
+
+  async comparePassword(
+    password: string,
+    passwordSql: string,
+  ): Promise<boolean> {
+    const hash = await bcrypt.compare(password, passwordSql);
+    return hash;
   }
 }
